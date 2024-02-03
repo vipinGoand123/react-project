09c2e136b9340503873d4d9e8e0191746461b3e5
Vipin Goand <vipingoand99@gmail.com>
2024-02-03 13:55:02 +0530

    changes

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-function MyApp(){
-  return(
-    <div>
-      <h1>Custom App</h1>
-    </div>
-  )
-}
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>Custom App</h1>
+//     </div>
+//   )
+// }
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
